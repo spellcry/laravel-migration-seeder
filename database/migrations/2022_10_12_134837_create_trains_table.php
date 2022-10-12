7ac21dba6f71078fa5e3e38cd34e30d06ede8c50
spellcry <sangiothebest@hotmail.com>
2022-10-12 16:08:50 +0200

    add Migration, Model, HomeController, View, Layout

---
+++ database/migrations/2022_10_12_134837_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->string('orario_partenza', 5);
+            $table->string('orario_arrivo', 5);
+            $table->unsignedSmallInteger('codice_treno');
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->unsignedTinyInteger('in_orario')->default(1);
+            $table->unsignedTinyInteger('cancellato')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/app')
+
+@section('metaTitle', 'Trains')
+
+@section('content')
+    <h1>Trains</h1>
+@endsection
